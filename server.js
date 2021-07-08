4760102ab0d25efc3822938e02eb69f77fa90dcd
AshleyThomas87 <77860087+AshleyThomas87@users.noreply.github.com>
2021-07-08 15:26:09 -0700

    server

--- server.js
+++ server.js
@@ -2,8 +2,11 @@
 projectData = {};
 
 // Require Express to run server and routes
+var express = require('express');
+var bodyParser = require('body-parser');
 
 // Start up an instance of app
+var app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,9 +14,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+var cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+var port = 1800;
+
+const server = app.listen(port, () => {
+    console.log(`server is listening on port: ${port}`); 
+});
--- website/app.js
+++ website/app.js
@@ -1,5 +1,14 @@
 /* Global Variables */
+const apikey = "8b2e2ace7c74f2124c371d00f8b5e0eb";
+const url = "api.openweathermap.org/data/2.5/weather?zip=";
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear(); 
+
+
+
+// eventListener for generate button
+// code for button and functionality
+// GET and POST
+// Generates data onto site
