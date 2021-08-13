85314c6c2dde412983ad8dde2412b0ab64a01fa0
AshleyThomas87 <77860087+AshleyThomas87@users.noreply.github.com>
2021-08-13 09:24:57 -0700

    updates

--- server.js
+++ server.js
@@ -21,9 +21,40 @@ app.use(cors());
 app.use(express.static('website'));
 
 
-// Setup Server
-var port = 1800;
 
-const server = app.listen(port, () => {
-    console.log(`server is listening on port: ${port}`); 
+// Setup Server
+const port = 1800;
+
+//const server = app.listen(port, () => {console.log(`server is listening on port: ${port}`); 
+const server = app.listen(port, () => { console.log(`running on localhost: ${port}`) 
+});
+
+
+// Return Endpoint Data
+// GET Route I: Server Side
+// There should be a GET route setup on the server side with the first argument as a string naming the route, and the second argument a callback function to return the JS object created at the top of server code.
+app.get('/', function (req, res) {
+    res.send('hello world');
+  })
+// POST
+// You should be able to add an entry to the project endpoint using a POST route setup on the server side and executed on the client side as an asynchronous function.
+// The server side function should create a new entry in the apps endpoint (the named JS object) consisting of the data received from the client side POST.
+app.post('/addAnimal', addAnimal);
+
+function addAnimal(req,res){
+
+  newEntry = {
+    animal: req.body.animal,
+    facts: req.body.fact,
+    fav: req.body.fav
+  }
+
+  animalData.push(newEntry)
+  console.log(animalData)
+}
+
+// // POST method route
+app.post('/add', function (request, response) {
+    let data = request.body;
+    console.log(data);
 });
--- website/app.js
+++ website/app.js
@@ -6,9 +6,54 @@ const url = "api.openweathermap.org/data/2.5/weather?zip=";
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear(); 
 
-
-
+//
 // eventListener for generate button
+// Adds an event listener to an existing HTML button from DOM using Vanilla JS.
+
+
 // code for button and functionality
-// GET and POST
+// In the file app.js, the element with the id of generate should have an addEventListener() method called on it, with click as the first parameter, and a named callback function as the second parameter.
+
+// GET 
+// setting up routes 
+// The API Key variable is passed as a parameter to fetch()
+// Data is successfully returned from the external API.
+
+// POST
+// Return Endpoint Data
+// GET Route II: Client Side
+// There should be an asynchronous function to fetch the data from the app endpoint
+// The client side function should take two arguments, the URL to make a POST to, and an object holding the data to POST.
+const postData = async ( url = '', data = {})=>{
+    console.log(data);
+      const response = await fetch(url, {
+      method: 'POST', 
+      credentials: 'same-origin',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+     // Body data type must match "Content-Type" header        
+      body: JSON.stringify(data), 
+    });
+
+      try {
+        const newData = await response.json();
+        console.log(newData);
+        return newData;
+      }catch(error) {
+      console.log("error", error);
+      }
+  }
+
+postData('/add', {answer:42});
 // Generates data onto site
+// Sets the properties of existing HTML elements from the DOM using Vanilla JavaScript.
+// Included in the async function to retrieve that app’s data on the client side, existing DOM elements should have their innerHTML properties dynamically set according to data returned by the app route.
+function postGet(){
+  postData('/animal', {fav:'lion'})
+    .then(function(data){
+      retrieveData('/all')
+    })
+}
+
+postGet()
